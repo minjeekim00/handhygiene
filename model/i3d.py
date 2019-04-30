e03e2e39c34f5a961f15c0298fc3bbd33d5145b0
minjee <minjeekim00@gmail.com>
2019-04-30 21:57:21 +0900

    added model and weights

---
+++ model/i3d.py
@@ -0,0 +1,122 @@
+import sys
+import torch.nn as nn
+from .i3dpt import I3D, Unit3Dpy
+from .i3dpt import *
+
+class I3D_binary(I3D):
+    def __init__(self,
+                 num_classes,
+                 modality='rgb',
+                 dropout_prob=0.5,
+                 name='inception'):
+        super(I3D, self).__init__()
+        
+        self.name = name
+        self.num_classes = num_classes
+        if modality == 'rgb':
+            in_channels = 3
+        elif modality == 'flow':
+            in_channels = 2
+        else:
+            raise ValueError(
+                '{} not among known modalities [rgb|flow]'.format(modality))
+        self.modality = modality
+        
+        
+        conv3d_1a_7x7 = Unit3Dpy(
+            out_channels=64,
+            in_channels=in_channels,
+            kernel_size=(7, 7, 7),
+            stride=(2, 2, 2),
+            padding='SAME')
+        # 1st conv-pool
+        self.conv3d_1a_7x7 = conv3d_1a_7x7
+        self.maxPool3d_2a_3x3 = MaxPool3dTFPadding(
+            kernel_size=(1, 3, 3), stride=(1, 2, 2), padding='SAME')
+        # conv conv
+        conv3d_2b_1x1 = Unit3Dpy(
+            out_channels=64,
+            in_channels=64,
+            kernel_size=(1, 1, 1),
+            padding='SAME')
+        self.conv3d_2b_1x1 = conv3d_2b_1x1
+        conv3d_2c_3x3 = Unit3Dpy(
+            out_channels=192,
+            in_channels=64,
+            kernel_size=(3, 3, 3),
+            padding='SAME')
+        self.conv3d_2c_3x3 = conv3d_2c_3x3
+        self.maxPool3d_3a_3x3 = MaxPool3dTFPadding(
+            kernel_size=(1, 3, 3), stride=(1, 2, 2), padding='SAME')
+
+        # Mixed_3b
+        self.mixed_3b = Mixed(192, [64, 96, 128, 16, 32, 32])
+        self.mixed_3c = Mixed(256, [128, 128, 192, 32, 96, 64])
+
+        self.maxPool3d_4a_3x3 = MaxPool3dTFPadding(
+            kernel_size=(3, 3, 3), stride=(2, 2, 2), padding='SAME')
+
+        # Mixed 4
+        self.mixed_4b = Mixed(480, [192, 96, 208, 16, 48, 64])
+        self.mixed_4c = Mixed(512, [160, 112, 224, 24, 64, 64])
+        self.mixed_4d = Mixed(512, [128, 128, 256, 24, 64, 64])
+        self.mixed_4e = Mixed(512, [112, 144, 288, 32, 64, 64])
+        self.mixed_4f = Mixed(528, [256, 160, 320, 32, 128, 128])
+
+        self.maxPool3d_5a_2x2 = MaxPool3dTFPadding(
+            kernel_size=(2, 2, 2), stride=(2, 2, 2), padding='SAME')
+
+        # Mixed 5
+        self.mixed_5b = Mixed(832, [256, 160, 320, 32, 128, 128])
+        self.mixed_5c = Mixed(832, [384, 192, 384, 48, 128, 128])
+
+        self.avg_pool = torch.nn.AvgPool3d((2, 7, 7), (1, 1, 1))
+        self.dropout = torch.nn.Dropout(dropout_prob)
+        
+        self.conv3d_0c_1x1 = self.modify_classifier()
+        self.sigmoid = nn.Sigmoid()
+        
+    def modify_classifier(self):
+        last_layer = Unit3Dpy(in_channels=1024,
+                              out_channels=400,
+                              kernel_size=(1, 1, 1),
+                              activation=None,
+                              use_bias=True,
+                              use_bn=False)
+        
+        add = Unit3Dpy(in_channels=400, 
+                         out_channels=self.num_classes, 
+                         kernel_size=(1, 1, 1),
+                         activation=None, 
+                         use_bias=True, use_bn=False)
+        branch_0 = nn.Sequential(last_layer, add)
+
+        return branch_0
+    
+    def forward(self, inp):
+        # Preprocessing
+        out = self.conv3d_1a_7x7(inp)
+        out = self.maxPool3d_2a_3x3(out)
+        out = self.conv3d_2b_1x1(out)
+        out = self.conv3d_2c_3x3(out)
+        out = self.maxPool3d_3a_3x3(out)
+        out = self.mixed_3b(out)
+        out = self.mixed_3c(out)
+        out = self.maxPool3d_4a_3x3(out)
+        out = self.mixed_4b(out)
+        out = self.mixed_4c(out)
+        out = self.mixed_4d(out)
+        out = self.mixed_4e(out)
+        out = self.mixed_4f(out)
+        out = self.maxPool3d_5a_2x2(out)
+        out = self.mixed_5b(out)
+        out = self.mixed_5c(out)
+        out = self.avg_pool(out)
+        out = self.dropout(out)
+        out = self.conv3d_0c_1x1(out)
+        out = out.squeeze(3)
+        out = out.squeeze(3)
+        out = out.mean(2)
+        out_logits = out
+        out = self.sigmoid(out_logits)
+        return out, out_logits
