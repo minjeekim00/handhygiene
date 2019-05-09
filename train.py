0d16a268f5494366664dab988e4ffc6b13f9517f
minjee <minjeekim00@gmail.com>
2019-05-09 14:31:42 +0900

    main code changed

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import time
 import copy
 import torch
 import torch.nn as nn
-from torch.optim import lr_scheduler
 from torch.autograd import Variable
 from model.i3dpt import I3D, Unit3Dpy
 
@@ -18,7 +17,7 @@ writer = SummaryWriter(logpath)
 
 rgb_weights_path = 'model/model_rgb.pth'
 flow_weights_path = 'model/model_flow.pth'
-optims={'rgb': None, 'flow':None}
+
 
 def set_param_requires_grad(model, feature_extracting):
     if feature_extracting:
@@ -54,7 +53,7 @@ def get_models(num_classes, feature_extract):
 
 
 
-def train(models, dataloaders, optimizer, criterion, device, num_epochs=50): 
+def train(models, dataloaders, optimizer, criterion, scheduler, device, num_epochs=50): 
     since = time.time()
     i3d_rgb, i3d_flow = models
     best_model_wts = {'rgb':i3d_rgb.state_dict(), 'flow':i3d_flow.state_dict()}
@@ -65,9 +64,13 @@ def train(models, dataloaders, optimizer, criterion, device, num_epochs=50):
         
         for phase in ['train', 'val']:
             if phase == 'train':
+                scheduler['rgb'].step()
+                scheduler['flow'].step()
                 i3d_rgb.train()
+                i3d_flow.train()
             else:
                 i3d_rgb.eval()
+                i3d_flow.eval()
 
             running_loss = 0.0
             running_corrects = {'rgb':0, 'flow':0, 'joint':0}
@@ -90,20 +93,23 @@ def train(models, dataloaders, optimizer, criterion, device, num_epochs=50):
                 flow_out_vars, flow_out_logits = i3d_flow(flows)
                 flow_preds = torch.round(flow_out_vars.data)
                 
-                ##### joint model
-                out_logit = rgb_out_logits + flow_out_logits
-                out_sigmoid = torch.sigmoid(out_logit)
-                print("out_sigmoid:{}".format(out_sigmoid.data))
-                out_preds = torch.round(out_sigmoid.data)
-                out_loss= criterion(out_sigmoid, targets).to(device)
+                with torch.set_grad_enabled(phase == 'train'):
+                    ##### joint model
+                    out_logit = rgb_out_logits + flow_out_logits
+                    out_sigmoid = torch.sigmoid(out_logit)
+                    out_preds = torch.round(out_sigmoid.data)
+                    out_loss= criterion(out_sigmoid, targets).to(device)
+                
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        out_loss.backward()
+                        optimizer['rgb'].step()
+                        optimizer['flow'].step()
                 
                 running_loss += out_loss.item() * rgbs.size(0)
                 running_corrects['rgb'] += torch.sum(rgb_preds.to(device) == targets.to(device)) 
                 running_corrects['flow'] += torch.sum(flow_preds.to(device) == targets.to(device)) 
                 running_corrects['joint'] += torch.sum(out_preds.to(device) == targets.to(device)) 
-
-                del rgbs, flows, out_loss, out_sigmoid
-                torch.cuda.empty_cache()
             
             ## for plotting 
             # per epoch
