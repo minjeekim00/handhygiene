bb2128851c18944b2ea303a9f71d8eb59da514b9
root <root@csi-cluster-gpu16.dakao.io>
2019-08-02 14:33:28 +0900

    added temporal augmentation

--- dataloader/handhygiene.py
+++ dataloader/handhygiene.py
@@ -1,42 +1,171 @@
-from torchvision.datasets.video_utils import VideoClips
-from torchvision.datasets.utils import list_dir
-from torchvision.datasets.folder import make_dataset
-from torchvision.datasets.vision import VisionDataset
+import torch
+import torch.utils.data as data
+from .i3ddataset import * # get_framepaths, get_flownames
+from .makedataset import make_hh_dataset
+from .makedataset import target_dataframe
+from .makedataset import get_keypoints
+
+import os
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from glob import glob
+import logging
+
+import sys
+sys.path.append('./utils/python-opencv-cuda/python')
+import common as cm
+
+def make_dataset(dir, class_to_idx, df, data, cropped):
+    exclusions = ['40_20190208_frames026493',
+                  '34_20190110_frames060785', #window
+                  '34_20190110_frames066161',
+                  '34_20190110_frames111213']
+    fnames, coords, labels = make_hh_dataset(dir, class_to_idx, df, data, exclusions, cropped)
+    targets = labels_to_idx(labels)
+    return [fnames, coords, targets]
 
 
-class HandHygiene(VisionDataset):
+def default_loader(fnames, coords, cropped):
+    rgbs = video_loader(fnames, coords)
+    flows = optflow_loader(fnames, coords, cropped)
+    return rgbs, flows
+
+
+def video_loader(fnames, coords):
     """
-    Args:
-        root (string): Root directory of the Dataset.
-        frames_per_clip (int): number of frames in a clip
-        step_between_clips (int): number of frames between each clip
-        transform (callable, optional): A function/transform that  takes in a TxHxWxC video
-            and returns a transformed version.
-    Returns:
-        video (Tensor[T, H, W, C]): the `T` video frames
-        label (int): class of the video clip
+        return: list of PIL Images
     """
+    video = []
+    for i, fname in enumerate(fnames):
+        with open(fname, 'rb') as f:
+            img = Image.open(f)
+            img = img.convert('RGB')
+            video.append(img)
+    return video
 
-    def __init__(self, root, frames_per_clip, step_between_clips=1, transform=None):
-        super(HandHygiene, self).__init__(root)
-        extensions = ('mp4',)
 
-        classes = list(sorted(list_dir(root)))
-        class_to_idx = {classes[i]: i for i in range(len(classes))}
-        self.samples = make_dataset(self.root, class_to_idx, extensions, is_valid_file=None)
-        self.classes = classes
-        video_list = [x[0] for x in self.samples]
-        self.video_clips = VideoClips(video_list, frames_per_clip, step_between_clips)
-        self.transform = transform
+def optflow_loader(fnames, coords, cropped):
+    """
+        return: list of PIL Images
+    """
+    isReversed=check_reverse(fnames)
+    ffnames = get_flownames(fnames, isReversed, cropped)
+    if any(not os.path.exists(f) for f in ffnames):
+        dir = os.path.split(fnames[0])[0]
+        #cal_for_frames(dir)
+        cm.findOpticalFlow(dir, True, True, isReversed, cropped)
+        
+    return video_loader(ffnames, coords)
 
-    def __len__(self):
-        return self.video_clips.num_clips()
 
-    def __getitem__(self, idx):
-        video, _, info, video_idx = self.video_clips.get_clip(idx)
-        label = self.samples[video_idx][1]
+def get_flownames(fnames, reversed, cropped):
+    ffnames=[]
+    for img in fnames:
+        dir = os.path.split(img)[0]
+        tail = os.path.split(img)[1]
+        name, ext = os.path.splitext(tail)
+        if len(dir.split('_'))>3: # for augmentated dir
+            dir = '_'.join(dir.split('_')[:-1])
+            
+        flowdirname='flow' if not reversed else 'reverse_flow'
+        flowdir=os.path.join(dir, flowdirname)
+        flow = os.path.join(flowdir, name+'_flow'+ext)
+        ffnames.append(flow)
+    return ffnames
 
-        if self.transform is not None:
-            video = self.transform(video)
 
-        return video,label
+def check_reverse(frames):
+    return True if frames != sorted(frames) else False
+
+    
+class HandHygiene(I3DDataset):
+        
+    def __init__(self, root, split='train', clip_len=16, 
+                 spatial_transform=None,
+                 temporal_transform=None,
+                 openpose_transform=None,
+                 target_transform=None,
+                 preprocess=False, loader=default_loader, num_workers=1, cropped=True):
+
+        super(HandHygiene, self).__init__(root, split, clip_len,
+                                         spatial_transform=spatial_transform,
+                                         temporal_transform=temporal_transform,
+                                         target_transform=target_transform,
+                                         preprocess=preprocess, loader=loader,
+                                         num_workers=num_workers)
+        
+        if cropped:
+            self.image_dir = os.path.join(root, 'cropped')
+        df = target_dataframe()
+        keypoints = get_keypoints()
+        folder = os.path.join(self.image_dir, split)
+        classes, class_to_idx = self._find_classes(folder)
+        
+        self.loader = loader
+        self.samples = make_dataset(folder, class_to_idx, df, keypoints, cropped)
+        self.openpose_transform = openpose_transform
+        self.cropped = cropped
+        ## check optical flow
+        if preprocess:
+            self.preprocess(num_workers)
+            
+    def __getitem__(self, index):
+        # loading and preprocessing.
+        
+        cropped = self.cropped
+        fnames= self.samples[0][index]
+        findices = get_framepaths(fnames)
+        coords= self.samples[1][index]
+        
+        logging.info("sample: {}".format(index))
+        if self.temporal_transform is not None:
+            ## TODO: applying reversed flow
+            findices, coords = self.temporal_transform(findices, coords)
+        clips, flows = self.loader(findices, coords, cropped)
+        
+        if self.openpose_transform is not None:
+            self.openpose_transform.randomize_parameters()
+            streams = [self.openpose_transform(img, flow, coords, i)
+                       for i, (img, flow) in enumerate(zip(clips, flows))]
+            if len(streams[0])==0:
+                print(self.__getpath__(index), "windows empty")
+                print(coords)
+            clips = [stream[0] for stream in streams]
+            flows = [stream[1] for stream in streams]
+        
+        if self.spatial_transform is not None:
+            self.spatial_transform.randomize_parameters()
+            clips = [self.spatial_transform(img) for img in clips]
+            flows = [self.spatial_transform(img) for img in flows]
+        
+        target = self.samples[2][index]
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        
+        target = torch.tensor(target).unsqueeze(0)
+        clips = torch.stack(clips).permute(1, 0, 2, 3)
+        flows = [flow[:-1,:,:] for flow in flows]
+        flows = torch.stack(flows).permute(1, 0, 2, 3)
+        
+        return clips, flows, target
+        #return clips, flows, target, coords
+        
+        
+    def preprocess(self, num_workers):
+        useCuda=True
+        cropped=True
+        paths = [self.__getpath__(i) for i in range(self.__len__()) 
+                     if check_cropped_dir(self.__getpath__(i))]
+        if not useCuda:
+            from multiprocessing import Pool
+            from .opticalflow import cal_for_frames
+            from .opticalflow import cal_reverse
+            
+            pool = Pool(num_workers)
+            pool.map(cal_for_frames, paths)
+            pool.map(cal_reverse, paths)
+            return
+        else:
+            for path in tqdm(paths):
+                cm.findOpticalFlow(path, useCuda, True, False, cropped)
--- dataloader/videodataset.py
+++ dataloader/videodataset.py
@@ -4,14 +4,14 @@ from torchvision.datasets.utils import list_dir
 from torchvision.datasets.folder import make_dataset
 from torchvision.datasets.vision import VisionDataset
 
-class VideoDataset(VisionDataset):
 
+class VideoDataset(VisionDataset):
+    
     def __init__(self, root, frames_per_clip, step_between_clips=1, 
                  spatial_transform=None,
                  temporal_transform=None):
-        super(HandHygiene, self).__init__(root)
+        super(VideoDataset, self).__init__(root)
         extensions = ('mp4',)
-
         classes = list(sorted(list_dir(root)))
         class_to_idx = {classes[i]: i for i in range(len(classes))}
         self.samples = make_dataset(self.root, class_to_idx, extensions, is_valid_file=None)
@@ -22,10 +22,9 @@ class VideoDataset(VisionDataset):
         self.spatial_transform = spatial_transform
         self.temporal_transform = temporal_transform
         
-
     def __len__(self):
         return self.video_clips.num_clips()
-
+    
     def __getitem__(self, idx):
         """
             video (Tensor[T, H, W, C]): the `T` video frames
@@ -33,12 +32,16 @@ class VideoDataset(VisionDataset):
         """
         video, _, _, video_idx = self.video_clips.get_clip(idx)
         label = self.samples[video_idx][1]
-
+        
+        if self.temporal_transform is not None:
+            self.temporal_transform.randomize_parameters()
+            video = [self.temporal_transform(img) for img in video]
+            
         video = [v.permute(2, 0, 1) for v in video] # for to_pil_image
         if self.spatial_transform is not None:
             self.spatial_transform.randomize_parameters()
             video = [self.spatial_transform(img) for img in video]
-
+            
         video = torch.stack(video).transpose(0, 1) # TCHW-->CTHW
         label = torch.tensor(label).unsqueeze(0) # () -> (1,)
         return video, label
