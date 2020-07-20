5188140b25c026c965e0605252807fa8afd58ffb
minjee <minjeekim00@gmail.com>
2020-07-20 15:38:56 +0900

    Modified dataloader for action detection task

--- dataloader/io/video.py
+++ dataloader/io/video.py
@@ -15,58 +15,118 @@ def get_frames(dirname):
                    for file in os.listdir(dirname) 
                    if is_image_file(file)])
 
-def read_video(dirname, start_pts=0, end_pts=None, has_bbox=False):
-    
+def read_video(dirname, start_pts=0, end_pts=None, has_bbox=False, downsample=None):
     frames = get_frames(dirname)
     video = []
+    
+    w = 0
+    h = 0
     for i, frame in enumerate(frames):
         with open(frame, 'rb') as f:
             img = Image.open(f)
             img = img.convert('RGB')
+            
+            if downsample:
+                w, h = np.asarray(img).shape[:2]
+                ratio = min(w/downsample, h/downsample)
+                w /= ratio
+                h /= ratio
+                img.thumbnail((h, w), Image.ANTIALIAS)
+                #img.save(frame, "PNG")
             img = np.asarray(img)
             video.append(img)
-            
+
     if end_pts is None:
         end_pts = len(video)
 
     if end_pts < start_pts:
         raise ValueError("end_pts should be larger than start_pts, got "
                          "start_pts={} and end_pts={}".format(start_pts, end_pts))
-        
+    
     video = np.asarray(video)
     video = torch.tensor(video)
     audio = torch.tensor([]) #tmp
-    info = {'video_fps': 15.0,
-           'body_keypoint': None}
+    info = {'width': w,
+            'height': h,
+            'video_fps': 15.0,
+           'keypoints': None,
+           'annotations': []}
     
     ## when has detection box in image frame
+    '''
     if has_bbox:
         fname = os.path.basename(dirname)
         txtfile = os.path.join(dirname, '{}.txt'.format(fname))
         with open(txtfile, 'r') as f:
             item = json.load(f)
         
-        
         ## this is to get body keypoint coordinates,
         ## otherwise skip
         if not True: #need_preprocess:
-            info['body_keypoint'] = item
+            info['keypoints'] = item
         else:
-            #print("item", item)
             coords = preprocess_keypoints(dirname, item)
-            #sprint("coords", coords)
-            info['body_keypoint'] = coords
-    
+            info['keypoints'] = coords
+
+        condition = len(coords['people']) == len(frames)
+        message = "{}: frames:{}, coords:{}".format(
+            dirname, len(frames), len(coords['people']))
+        assert condition, message
+    '''
+    """
+    if has_bbox:
+        from natsort import natsorted
+        fname = os.path.basename(dirname)
+        jsonfiles = natsorted(glob(os.path.join(dirname, '*.json')))
+        
+        for jsonfile in jsonfiles:
+            '''
+            bboxes = {{person_id_1: [[x1, y1, x2, y2], [label]] 
+                       person_id_2: [[x1, y1, x2, y2], [label]]}}
+            '''
+            bboxes = _get_bounding_box(fname, jsonfile, bbox_type='labelme')
+            info['annotations'].append(bboxes) 
+            
+            # TODO: confidence score thresholding
+    """
+            
+    if has_bbox:
+        for frame in frames:
+            image_path = os.path.splitext(os.path.basename(frame))[0]
+            bboxes = _get_bbox_info(image_path)
+            info['annotations'].append(bboxes)
+
+    #info['annotations'] = _get_completed_annotations(info['annotations'])
     sample = (video, audio, info)
     return read_video_as_clip(sample, start_pts, end_pts, has_bbox)
 
-def read_video_as_clip(sample, start_pts, end_pts, has_bbox):
+    
+def target_dataframe(path='./data/annotations/hh_target.csv'):
+    import pandas as pd
+    df = pd.read_csv(path)
+    df = df[df['person_id'].notnull()] # target 있는 imgpath만 선별
+    return df
+
+    
+def read_video_as_clip(sample, start_pts, end_pts, has_bbox, target=None):
     video, audio, info = sample
     video = video[start_pts:end_pts+1]
     
-    ##TODO: slicing keypoints
-    #info['body_keypoint'] = info['body_keypoint'][start_pts:end_pts+1]
-    #print(info)
+    if end_pts == len(video):
+        return (video, audio, info)
+    
+    if has_bbox:
+        ##TODO: slicing keypoints
+        #info['keypoints'] = info['keypoints'][start_pts:end_pts+1]
+        info_tmp = info.copy()
+        info_tmp['annotations'] = info_tmp['annotations'][start_pts:end_pts+1]
+        
+        if target is not None:
+            info_tmp['annotations'] = _get_target_annotations(info_tmp['annotations'], target)
+        
+        #info_tmp['annotations'] = _get_completed_annotations(info_tmp['annotations'])
+        return (video, audio, info_tmp)
+    
     return (video, audio, info)
 
 
@@ -76,13 +136,152 @@ def read_video_timestamps(dirname):
     return (list(range(len(frames)*1)), 15.0)
 
 
-def target_dataframe(path='./data/label.csv'):
-    df=pd.read_csv(path)
-     # target 있는 imgpath만 선별
-    df = df[df['targets'].notnull()]
-    return df
+def _get_bbox_info(image_path, df=target_dataframe()):
+    
+    if 'flow' in image_path:
+        image_path = image_path.replace('_flow', '')
+    rows = df[df['image_path']==image_path]
+    assert len(rows) > 0, image_path
+    
+    box_dict = {}
+    for row in rows.values:
+        _, _, label, person_id, x1, y1, x2, y2 = row
+        box_dict[person_id] = [[x1, y1, x2, y2], label]
+    return box_dict
+
+######################################################################################################
 
 
+def _get_action_name(fname, person_id, df=target_dataframe()):
+    """ return action label from data frame """
+    row = df[(df['clip_name'] == fname) & (df['person_id'] == person_id)]
+    assert len(row) <= 1, "There is(are) {} labels on person {} in {}".format(len(row), person_id, fname)
+    
+    if len(row) == 1:
+        # TODO:
+        if row['action_name'].values[0] == 'touching_patient':
+            return 'other_action'
+        else:
+            return row['action_name'].values[0]
+    else: # no label
+        return 'other_action'
+    
+    
+def _get_bounding_box(fname, jsonfile, bbox_type='labelme'):
+    ''' feeding action label here '''
+    
+    import json
+    if bbox_type == 'labelme':
+        bboxes = {}
+        
+        with open(jsonfile, 'r') as f:
+            item = json.load(f)
+
+            assert os.path.splitext(item['imagePath'])[0] == os.path.basename(os.path.splitext(jsonfile)[0])
+
+            for person in item['shapes']:
+                person_id = person['group_id']
+                bbox = list(np.array(person['points']).reshape((4)))
+
+                if person_id not in bboxes:
+                    label = _get_action_name(fname, person_id)
+                    bboxes[person_id] = [bbox, label]
+    #TODO: else
+    return bboxes
+
+
+def _get_person_ids(annotations):
+    person_ids = []
+    for ann in annotations:
+        for person_id in ann.keys():
+            if person_id not in person_ids:
+                person_ids.append(person_id)
+    return sorted(person_ids)
+
+def _get_completed_annotations(annotations):
+    """Grab only completed annotations throughout the video clip"""
+    person_ids = _get_person_ids(annotations)
+    for person_id in person_ids:
+        
+        if all([person_id in ann.keys() for ann in annotations]):
+            continue
+        
+        # pop from each annotations
+        for ann in annotations:
+            if person_id in ann.keys():
+                ann.pop(person_id)
+    return annotations
+
+def _get_target_annotations(annotations, target):
+    """Grab only completed annotations throughout the video clip"""
+    
+    info_new = []
+    for ann in annotations:
+        ann_new = {}
+        for person_id in ann.keys():
+            if person_id == target:
+                ann_new[person_id] = ann[person_id]
+        info_new.append(ann_new)
+    return info_new
+
+'''
+def create_new_dataframe(dirname):
+    import json
+    from natsort import natsorted
+    import pandas as pd
+    
+    info = {'annotations': []}
+    
+    fname = os.path.basename(dirname)
+    jsonfiles = natsorted(glob(os.path.join(dirname, '*.json')))
+
+    for jsonfile in jsonfiles:
+        bboxes = _get_bounding_box(fname, jsonfile, bbox_type='labelme')
+        info['annotations'].append(bboxes)
+        
+    info['annotations'] = _get_completed_annotations(info['annotations'])
+    
+    vids = []
+    frame_names = []
+    labels = []
+    peson_ids = []
+    x1s = []
+    y1s = []
+    x2s = []
+    y2s = []
+
+    for phase in ['train', 'val', 'test']:
+
+        for dirname in tqdm(glob('./data/images_new/{}/*'.format(phase))[:]):
+
+            info = read_video(dirname, has_bbox=True)
+            fname = os.path.basename(dirname)
+            vid = int(fname.split('_')[0])
+            jsonfiles = natsorted(glob(os.path.join(dirname, '*.json')))
+
+            for aidx, ann in enumerate(info['annotations']):
+                for key, value in ann.items():
+                    person_id = key
+                    x1, y1, x2, y2 = value[0]
+                    label = value[1]
+                    frame_name = os.path.splitext(os.path.basename(jsonfiles[aidx]))[0]
+
+                    vids.append(vid)
+                    frame_names.append(frame_name)
+                    labels.append(label)
+                    peson_ids.append(person_id)
+                    x1s.append(x1)
+                    y1s.append(y1)
+                    x2s.append(x2)
+                    y2s.append(y2)
+                    
+    
+    li = [frame_names, labels, peson_ids, x1s, y1s, x2s, y2s]
+    df = pd.DataFrame(vids, columns = ['video_id'])
+    for cid, column in enumerate(['image_path', 'action', 'person_id', 'x1', 'y1', 'x2', 'y2']):
+        df[column] = li[cid]
+'''
+'''
 def preprocess_keypoints(dirname, item, df=target_dataframe()):
     fname = dirname.split('/')[-1]
     label = dirname.split('/')[-2]
@@ -120,3 +319,4 @@ def preprocess_keypoints(dirname, item, df=target_dataframe()):
 #                       'torso':torso[tidx][start:end]}
             
     return coords
+'''
--- dataloader/video_utils.py
+++ dataloader/video_utils.py
@@ -7,6 +7,8 @@ import torch
 from dataloader.io.video import read_video_timestamps
 from dataloader.io.video import read_video_as_clip
 from dataloader.io.video import read_video
+from dataloader.io.video import get_frames
+from dataloader.io.video import _get_bbox_info
 from torchvision.datasets.utils import tqdm
 
 
@@ -25,20 +27,19 @@ def unfold(tensor, size, step, dilation=1):
 
 class VideoClips(object):
     def __init__(self, video_paths, clip_length_in_frames=16, frames_between_clips=1,
-                 frame_rate=None, _precomputed_metadata=None, with_detection=False):
+                 frame_rate=None, _precomputed_metadata=None, with_detection=False,
+                  downsample_size=None):
         self.video_paths = video_paths
         if _precomputed_metadata is None:
             self._compute_frame_pts()
         else:
             self._init_from_metadata(_precomputed_metadata)
         self.compute_clips(clip_length_in_frames, frames_between_clips, frame_rate)
-        self.cached_data = [None for i in range(len(video_paths))]
         
-        ## to cache all data when initializing
-#         has_bbox = False
-#         if 'flow' not in self.video_paths[0]:
-#             has_bbox = True
-#         self.cached_data = [read_video(video_path, 0, None, has_bbox) for video_path in self.video_paths]
+        self.downsample_size = downsample_size
+
+        self.shared_manager = None
+        self.shared_data = [None for video_path in self.video_paths]
         self.with_detection = with_detection
 
     def _compute_frame_pts(self):
@@ -120,18 +121,37 @@ class VideoClips(object):
         self.frame_rate = frame_rate
         self.clips = []
         self.resampling_idxs = []
+        self.info = []
         for vidx, (video_pts, fps) in enumerate(zip(self.video_pts, self.video_fps)):
             
             # customize step per class
-            _, _, label, _ = self._split_path(self.video_paths[vidx])
-            step = steps[label]
+            if True: #self.with_detection:
+                # TODO
+                frame = get_frames(self.video_paths[vidx])[0]
+                image_path = os.path.splitext(os.path.basename(frame))[0]
+                bbox = _get_bbox_info(image_path)
+                
+                for i, (k, v )in enumerate(bbox.items()):
+                    label = v[1]
+                    step = steps[label]
+                    clips, idxs = self.compute_clips_for_video(video_pts, num_frames, step, fps, frame_rate)
+                    
+                    self.clips.append(clips)
+                    self.resampling_idxs.append(idxs)
+                    
+                    for cidx in range(len(clips)):
+                        #TODO:
+                        self.info.append([vidx, i, k, cidx, label])
+            else:
+                # get clip info
+                _, _, label, name = self._split_path(self.video_paths[vidx])
+                step = steps[label]
+                clips, idxs = self.compute_clips_for_video(video_pts, num_frames, step, fps, frame_rate)
+                self.clips.append(clips)
+                self.resampling_idxs.append(idxs)
             
-            clips, idxs = self.compute_clips_for_video(video_pts, num_frames, step, fps, frame_rate)
-            self.clips.append(clips)
-            self.resampling_idxs.append(idxs)
         clip_lengths = torch.as_tensor([len(v) for v in self.clips])
         self.cumulative_sizes = clip_lengths.cumsum(0).tolist()
-
         
     def _split_path(self, path):
         if 'flow' in path: # for optical flow dir
@@ -141,6 +161,7 @@ class VideoClips(object):
         root, label = os.path.split(root)
         root, phase = os.path.split(root)
         return root, phase, label, name
+#         return root, phase, name
         
     def __len__(self):
         return self.num_clips()
@@ -153,19 +174,31 @@ class VideoClips(object):
         Number of subclips that are available in the video list.
         """
         return self.cumulative_sizes[-1]
+    
+    def num_total_frames(self):
+        return sum([len(get_frames(path)) for path in self.video_paths])
+    
+    def num_image_frames(self, path):
+        return len(get_frames(path))
 
     def get_clip_location(self, idx):
         """
         Converts a flattened representation of the indices into a video_idx, clip_idx
         representation.
         """
+        '''
         video_idx = bisect.bisect_right(self.cumulative_sizes, idx)
         if video_idx == 0:
             clip_idx = idx
         else:
             clip_idx = idx - self.cumulative_sizes[video_idx - 1]
-        return video_idx, clip_idx
-
+        return video_idx, clip_idx  
+        '''
+        return self.info[idx], bisect.bisect_right(self.cumulative_sizes, idx)
+                                            
+    def get_video_idx(self, idx):
+        return bisect.bisect_right(self.cumulative_sizes, idx)
+    
     @staticmethod
     def _resample_video_idx(num_frames, original_fps, new_fps):
         step = float(original_fps) / new_fps
@@ -178,22 +211,42 @@ class VideoClips(object):
         idxs = idxs.floor().to(torch.int64)
         return idxs
 
+    def cache_video(self, video_idx, video):
+        """ cache video """
+        if 'flow' in self.video_paths[video_idx]:
+            print("[Flow] Filling cache for index {}".format(video_idx))
+        else:
+            print("[RGB] Filling cache for index {}".format(video_idx))
+        self.shared_data[video_idx] = video
+
+    def get_video(self, video_idx):
+        """ get video from shared data """
+        video_path = self.video_paths[video_idx]
+        
+        if self.shared_data[video_idx] is None:
+            video = read_video(video_path, 0, None, self.with_detection, self.downsample_size)
+            self.cache_video(video_idx, video)
+                
+        return self.shared_data[video_idx]
+    
     def get_clip(self, idx):
         if idx >= self.num_clips():
             raise IndexError("Index {} out of range "
                              "({} number of clips)".format(idx, self.num_clips()))
-        video_idx, clip_idx = self.get_clip_location(idx)
+        #video_idx, clip_idx = self.get_clip_location(idx)
+        (video_idx, idx, person_idx, clip_idx, label), video_c = self.get_clip_location(idx)
         video_path = self.video_paths[video_idx]
-        clip_pts = self.clips[video_idx][clip_idx]
+        
+        #print("video_idx {}, idx {}, video_c {}, clip_idx {}".format(video_idx, idx, video_c, clip_idx))
+        clip_pts = self.clips[video_c][clip_idx]
         start_pts = clip_pts[0].item()
         end_pts = clip_pts[-1].item()
-        has_bbox = self.with_detection
         
-        if self.cached_data[video_idx] is None:
-            self.cached_data[video_idx] = read_video(video_path, 0, None, has_bbox)
-        video, audio, info = read_video_as_clip(self.cached_data[video_idx], 
-                                                start_pts, end_pts, has_bbox)
+        video_raw = self.get_video(video_idx)
+        video, audio, info = read_video_as_clip(video_raw, start_pts, end_pts, self.with_detection, target=person_idx)
+        
         if self.frame_rate is not None:
+            #TODO:
             resampling_idx = self.resampling_idxs[video_idx][clip_idx]
             if isinstance(resampling_idx, torch.Tensor):
                 resampling_idx = resampling_idx - resampling_idx[0]
@@ -201,4 +254,15 @@ class VideoClips(object):
             info["video_fps"] = self.frame_rate
         #if len(video) < self.num_frames:
         #    print("{} x {}".format(video.shape, self.num_frames))
+        
+        info['target_id'] = person_idx
+        info['label']= label
         return video, audio, info, video_idx
+
+    def cache_all(self):
+        self.shared_data = [self.get_video(video_idx) for video_idx 
+                            in tqdm(range(len(self.video_paths)))]
+    
+    def set_shared_manager(self, manager):
+        self.shared_manager = manager
+        self.shared_data = manager.list([None for video_path in self.video_paths])
